6ec95f5c0fd97c037e3bc7679ecde353f5d5c61b
xaazias <flamesuspect@gmail.com>
2022-08-14 19:52:44 +0300

    Added Separator component animation.

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,10 +1,35 @@
 import React from "react"
 import styles from "@styles/Separator.module.scss"
+import { motion, useScroll, useSpring } from "framer-motion"
 
 const Separator = () => {
+  const ref = React.useRef(null)
+  const [progress, setProgress] = React.useState(0)
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"]
+  })
+
+  const h = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  })
+
+  React.useEffect(() => {
+    return h.onChange((latest) => {
+      setProgress(latest)
+    })
+  }, [])
+
   return (
     <div className={styles.separator}>
-      <div className={styles.active}></div>
+      <motion.div
+        className={styles.active}
+        style={{ height: `${progress * 100}%` }}
+        ref={ref}
+      ></motion.div>
       <div className={styles.default}></div>
     </div>
   )
