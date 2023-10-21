43f5d461cb17f7be7744e21dfa47bc82af7f10d3
saizaax <flamesuspect@gmail.com>
2023-10-21 17:01:38 +0300

    fix: updated bio

--- src/components/BioCharacteristics.tsx
+++ src/components/BioCharacteristics.tsx
@@ -6,17 +6,25 @@ import { BioItem } from "@components/BioItem"
 const BioCharacteristics = () => {
   return (
     <div className={styles.characteristics}>
-      <BioItem name="Age" value="21 years old" />
+      <BioItem name="Age" value="22 years old" />
       <BioItem name="Location" value="Russia, Moscow" />
       <BioItem
         name="Education"
         value="RTU MIREA University, Software Engineering Bachelor's degree"
       />
       <BioItem name="Additional Education" value="Yandex SHRI Autumn 2021" />
-      <BioItem name="Total Experience" value="1+ year" />
+      <BioItem name="Total Experience" value="3.5+ years" />
       <BioItem
-        name="Work"
-        value="RTUITLab • April 2021 — Current"
+        name="Work 01"
+        value="RTUITLab • April 2020 — November 2021"
+      />
+      <BioItem
+        name="Work 02"
+        value="Mind Rhythms, LLC • November 2021 — December 2022"
+      />
+      <BioItem
+        name="Current Work"
+        value="Black Wall Group • December 2022 — Current"
         isLast={true}
       />
     </div>
